78dd4f64acef341b349ace379d488acdc2123a0f
Maxim Melnichenko <melnichenkomaxim97@gmail.com>
2023-04-29 15:24:42 +0300

    feat: low and high EN keys are made. Made changing the case of letters when pressing the shift key

--- app.js
+++ app.js
@@ -7,29 +7,54 @@ window.addEventListener('DOMContentLoaded', () => {
   const wrapper = document.querySelector('.wrapper'),
     keyboard = document.querySelector('.keyboard');
 
-  let keyboardArr = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '\\', 'Enter', 'Shift', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+  let lowEnKeysArr = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#9650;', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'Control', '&#9668;', '&#9660;', '&#9658;'];
 
-  function createKeys(keyboardArr) {
-    for (let i = 0; i < keyboardArr.length; i++) {
-      keyboard.innerHTML += `<div class = "key" data = ${keyboardArr[i]}>${keyboardArr[i]}</div>`;
+  let highEnKeysArr = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\|', 'Delete', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '&#9650;', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'Control', '&#9668;', '&#9660;', '&#9658;'];
+
+
+
+  let atributeArr = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+
+  function createKeys(arr, atribute) {
+    for (let i = 0; i < arr.length; i++) {
+      keyboard.innerHTML += `<div class = "key" data = ${atribute[i]}>${arr[i]}</div>`;
     }
   }
 
-  createKeys(keyboardArr)
+  createKeys(lowEnKeysArr, atributeArr)
 
   document.addEventListener('keydown', (event) => {
-   document.querySelector(`.key[data='${event.key}']`).classList.add('active');
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+      keyboard.innerHTML = '';
+      createKeys(highEnKeysArr, atributeArr)
+    } 
+      document.querySelector(`.key[data='${event.code}']`).classList.add('active');
+    
   })
 
   document.addEventListener('keyup', (event) => {
-   document.querySelector(`.key[data='${event.key}']`).classList.remove('active');
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+      keyboard.innerHTML = '';
+      createKeys(lowEnKeysArr, atributeArr)
+    } 
+    document.querySelector(`.key[data='${event.code}']`).classList.remove('active');
   })
-})
-  // let keyboardArr = []
+
+
+
+
+
+
+  // let keysArr = [];
+  // let atributeArr = [];
 
   //   document.addEventListener('keydown', (event) => {
   //     console.log(event);
 
-  //     keyboardArr.push(event.key);
-  //     console.log(keyboardArr);
+  //     atributeArr.push(event.code);
+  //     keysArr.push(event.key);
+  //     // console.log(atributeArr);
+  //     console.log(keysArr)
   //   })
+
+})
